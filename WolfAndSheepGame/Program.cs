b020191c3b114559571e61ada14a26a1c4b85e18
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-04-10 15:18:48 +0100

    Add project files.

---
+++ WolfAndSheepGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WolfAndSheepGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
